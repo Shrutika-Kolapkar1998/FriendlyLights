0ca997fd1e6a1ad7c615fc66829606799095eb96
Shrutika Kolapkar <51847942+Shrutika-Kolapkar1998@users.noreply.github.com>
2025-08-20 16:10:35 +0530

    added src files

--- FriendlyLights-old/src/App.js
+++ FriendlyLights-old/src/App.js
@@ -5,20 +5,31 @@ import ProjectsSection from "./components/ProjectsSection";
 import Footer from "./components/Footer";
 import ContactSection from "./components/ContactSection";
 import PortfolioSection from "./components/PortfolioSection";
-
-
-
+import AboutSection from "./components/AboutSection";
+import ProdDetail from "./components/ProdDetail";
+import FAQ from "./components/FAQ";
+import TestimonialCarousel from "./components/TestimonialCarousel";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <HeroSection />
-      <ProjectsSection />
-      <PortfolioSection />
-      <ContactSection />
-      <Footer />
-    </div>
+    <>
+        <div className="container-fluid p-0">
+          <Header/>
+        </div>
+        <div className="container-xxl">
+          <HeroSection />
+          <ProjectsSection />
+          <AboutSection />
+          <PortfolioSection />
+          <ProdDetail />
+          <TestimonialCarousel />
+          <FAQ />
+          <ContactSection />
+          <Footer />
+        </div>
+    </>
   );
 }
 
